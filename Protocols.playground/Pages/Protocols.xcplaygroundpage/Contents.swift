f37e77ff879a1c965a34f9b183ae8b6e890a6bbb
Matthew Harding <matthewpharding@gmail.com>
2022-10-16 11:08:34 +0100

    Added the page number (from official Swift documentation) to each Xcode playground file.

--- Protocols.playground/Pages/Protocols.xcplaygroundpage/Contents.swift
+++ Protocols.playground/Pages/Protocols.xcplaygroundpage/Contents.swift
@@ -1,6 +1,6 @@
 //: ![Swift](swift-logo.png)
 //: Swift v5.7 [official docs](https://docs.swift.org/swift-book/LanguageGuide/Protocols.html)
-//: # Protocols
+//: # 22. Protocols
 //:
 //:
 //: A protocol defines a blueprint of methods, properties, and other requirements that suit a particular task or piece of functionality. The protocol can then be adopted by a class, structure, or enumeration to provide an actual implementation of those requirements. Any type that satisfies the requirements of a protocol is said to conform to that protocol.
